9c255202b76cb769d84eb185bf401ade08c1c22d
valmit <sliper98@gmail.com>
2019-01-24 11:46:40 +0700

    some letters

--- source/graphical.cpp
+++ source/graphical.cpp
@@ -1,10 +1,23 @@
 #include "graphical.h"
 
 
+
 painter::painter(){
     con_init();
+    con_initPair(wight_on_black, CON_COLOR_WHITE, CON_COLOR_BLACK);
 
 }
+
+
+
+void painter::draw_board(std::vector<std::vector<tile>>& tiles){
+    for (size_t i = 0; i < tiles.size(); ++i) {
+        for (size_t j = 0; j < tiles.begin()->size(); ++j) {
+            tiles[i][j].draw();
+        }
+    }
+}
+
 
 painter::~painter() {
     con_deinit();
